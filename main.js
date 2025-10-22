9f7a09ef0bd715441b8abc7b3b8ab299687ae949
Jonathan Jackson <jonathan@mydell.noise>
2025-10-22 03:45:28 -0700

    created a function to get form values

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ document.getElementById('contactForm').addEventListener('submit', submitForm);
 function submitForm(e){
     e.preventDefault();
 
-    console.log(123);
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+    console.log(name, company, email, phone, message);
 }
 
+
+
+//Function to get form values
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
